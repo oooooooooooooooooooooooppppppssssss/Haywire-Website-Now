62e31861df56153e879579d53827f4b816028cdf
oooooooooooooooooooooooppppppssssss <39245125+oooooooooooooooooooooooppppppssssss@users.noreply.github.com>
2020-01-23 11:02:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,9 @@ class index extends React.Component {
                     Send submissions to</p>  <p class="contribute" style="font-size:small;>haywire@jfks.me</p>
 									
 								</h5>
-											<p className='center '>
+											<p className="center " >
 									<a
-										className='button button-orange largeButton '
+										className="button button-orange largeButton "
 										onClick={() => this.startTheWiggle()}
 										href={Edition.url}
 										download={'Haywire Issue ' + Edition.issueNumber}
