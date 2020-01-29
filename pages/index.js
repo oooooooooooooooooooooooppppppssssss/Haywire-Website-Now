25b654c714108b8ad295092bfe234757f8a792ca
oooooooooooooooooooooooppppppssssss <39245125+oooooooooooooooooooooooppppppssssss@users.noreply.github.com>
2020-01-29 16:44:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,8 +41,9 @@ class index extends React.Component {
 										We feature original student work of many kinds: short stories, vignettes, poems, critical essays, photographs, paintings, or comics.
          </p>
 									<p className="noBottom">Please submit your art to our magazine.
-                    Send submissions to  <p class="contribute" style="font-size:small;>haywire@jfks.me</p>
-									
+                                        Send submissions to <a href="mailto:haywire@jfks.me">haywire@jfks.me</a>
+									</p>
+
 								</h5>
 											<p className="center">
 									<a
