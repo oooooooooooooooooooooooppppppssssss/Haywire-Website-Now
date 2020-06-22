98e790659b99f8c22d264fbc96809ca4a76bdc0a
James Gromis <dagwoodsandwich56@gmail.com>
2020-06-22 17:17:25 +0200

    BLM Journal Update

--- edition.js
+++ edition.js
@@ -1,7 +1,7 @@
 const Edition = {
-	issue: 'Fourteen',
-	issueNumber: 14,
-	nextEditionDueDate: 'Spring 2020',
-	url: 'https://haywire.now.sh/static/HW14.pdf'
+	issue: 'BLM Journal',
+	issueNumber: 15,
+	nextEditionDueDate: 'Fall 2020',
+	url: 'https://haywire.now.sh/static/blmjournal.pdf'
 };
 export default Edition;
