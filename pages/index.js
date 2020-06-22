a0cc134d99c68d2a70529f28b173204767fa0a99
James Gromis <dagwoodsandwich56@gmail.com>
2020-06-22 21:41:54 +0200

    A Doozie

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,9 @@ class index extends React.Component {
 		return (
 			<Layout title="haywire">
 				<div className="section a">
-
+				<video autoplay muted loop id="backgroundvideo">
+				  <source src="backgroundvideo.mp4" type="video/mp4">
+				</video>
 					<div className="container " >
 						<div className="row">
 							<div class="bg-image img1"></div>
