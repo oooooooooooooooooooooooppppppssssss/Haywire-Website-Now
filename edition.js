6be33b66d722259d7a00769154cfcd4cfefc758c
oooooooooooooooooooooooppppppssssss <39245125+oooooooooooooooooooooooppppppssssss@users.noreply.github.com>
2020-09-16 16:47:49 +0200

    Update edition.js

--- edition.js
+++ edition.js
@@ -1,7 +1,7 @@
 const Edition = {
 	issue: 'Fourteen',
 	issueNumber: 14,
-	nextEditionDueDate: 'Spring 2020',
+	nextEditionDueDate: 'Wednesday, November 18',
 	url: 'https://haywire.now.sh/static/HW14.pdf'
 };
 export default Edition;
